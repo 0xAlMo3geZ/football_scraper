a770f6a6ca33631f590b982aecc63c843f2cd02b
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-01-05 17:23:19 +0200

    Update football_scraper.py

--- football_scraper.py
+++ football_scraper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-date = input("Please Enter a Game Day *MM/DD/YYYY*: ")
+date = input("Please Enter a Match Day *MM/DD/YYYY*: ")
 page = requests.get(
     f"https://www.yallakora.com/match-center/%D9%85%D8%B1%D9%83%D8%B2-%D8%A7%D9%84%D9%85%D8%A8%D8%A7%D8%B1%D9%8A%D8%A7%D8%AA?date={date}")
 
